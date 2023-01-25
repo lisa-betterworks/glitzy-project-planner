f4c94a0e674457b04be4752a68b870801f7955ce
Lisa Copeland <lisa.copeland@betterworks.com>
2023-01-24 21:32:17 -0500

    added dynamic data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- check for data in array -->
+    <div v-if="projects.length">
+      <!-- loop through the data -->
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
