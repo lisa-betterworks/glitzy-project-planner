46634238a874e775d91444cde54bc94f0848d81c
Lisa Copeland <lisa.copeland@betterworks.com>
2023-01-25 15:29:58 -0500

    added data and delete functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="home">
-    <!-- check for data in array -->
     <div v-if="projects.length">
-      <!-- loop through the data -->
       <div v-for="project in projects" :key="project.id">
-        <p>{{ project.title }}</p>
+        <SingleProject :project="project" @delete="handleDelete" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import SingleProject from "../components/SingleProject.vue";
+
 export default {
   name: "HomeView",
-  components: {},
+  components: { SingleProject },
   data() {
     return {
       projects: [],
@@ -24,6 +24,13 @@ export default {
       .then((res) => res.json())
       .then((data) => (this.projects = data))
       .catch((err) => console.log(err.message));
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id;
+      });
+    },
   },
 };
 </script>
