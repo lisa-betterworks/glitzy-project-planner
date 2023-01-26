c3c689d1f439711ff7ee73e8088ab29a7df3df72
Lisa Copeland <lisa.copeland@betterworks.com>
2023-01-26 15:29:33 -0500

    added new project functionality

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
       <div class="icons">
         <span class="material-icons"> edit </span>
         <span class="material-icons" @click="deleteProject"> delete </span>
-        <span class="material-icons"> done </span>
+        <span class="material-icons tick" @click="toggleComplete"> done </span>
       </div>
     </div>
     <div v-if="showDetails" class="details">
@@ -27,7 +27,18 @@ export default {
     deleteProject() {
       fetch(this.uri, { method: "DELETE" })
         .then(() => this.$emit("delete", this.project.id))
-        .cathch((err) => console.log(err.message));
+        .catch((err) => console.log(err.message));
+    },
+    toggleComplete() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ complete: !this.project.complete }),
+      })
+        .then(() => {
+          this.$emit("complete", this.project.id);
+        })
+        .catch((err) => console.log(err));
     },
   },
 };
@@ -58,5 +69,11 @@ h3 {
 }
 .material-icons:hover {
   color: #777;
+}
+.project.complete {
+  border-left: 4px solid #00ce89;
+}
+.project.complete .tick {
+  color: #00ce89;
 }
 </style>
